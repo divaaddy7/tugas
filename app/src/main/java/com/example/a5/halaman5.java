0858a02fd7ec52b4607b0598f95246a1808ef445
divaaddy7 <divaaddy7@gmail.com>
2024-04-24 22:18:52 +0700

    first commit

---
+++ app/src/main/java/com/example/a5/halaman5.java
@@ -0,0 +1,14 @@
+package com.example.a5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class halaman5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman5);
+    }
+}
